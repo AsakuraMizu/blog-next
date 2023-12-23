86f4432efa352d4096ea3e0a5b975d680a826667
AsakuraMizu <asakuramizu111@gmail.com>
2023-12-23 23:50:07 +0800

    upgrade

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import { h } from 'hastscript';
 import { mdsvex } from 'mdsvex';
 import rehypeAutolinkHeadings from 'rehype-autolink-headings';
